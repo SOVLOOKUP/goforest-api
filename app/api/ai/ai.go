20f655ff941dc1be766c6d3e7a5a5893a134f982
sovlookup <805408477@qq.com>
2020-05-30 23:15:45 +0800

    fix noroutebug

--- app/api/ai/ai.go
+++ app/api/ai/ai.go
@@ -1,15 +1,16 @@
 package ai
 
 import (
+	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
-	"github.com/gogf/gf/os/glog"
 )
 
 // Ai 动态路由解析
 func Ai(r *ghttp.Request) {
 	// if name := genv.Get(r.GetString("name"));name != "" {
 	if rq, err := ghttp.Post("http://"+r.GetString("pth"), r.GetBody()); err != nil {
-		glog.Error(err)
+		//glog.Debug(err.Error())
+		r.Response.WriteJsonExit(g.Map{"code":"0","msg":"router found","detail":err.Error()})
 	} else {
 		defer rq.Close()
 		r.Response.WriteJsonExit(rq.ReadAll())
